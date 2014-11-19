d884808ade50f973c4ac3391d6ddf28617cba248
Snorri <vaskur_@hotmail.com>
2014-11-19 01:38:39 +0000

    tók út tók sem virkaði ekki

--- js/notification.js
+++ js/notification.js
@@ -24,41 +24,33 @@ function Notification(type,scale){
 			break;
 		case "GOOD":
 			this.activeSprite = this.sprite.good;
-			g_FIREBOLTS = 3;
 			break;
 		case "SWEET":
 			this.activeSprite = this.sprite.sweet;
-			g_FIREBOLTS = 5;
 			break;
 		case "GREAT":
 			this.activeSprite = this.sprite.great;
-			g_FIREBOLTS = 8;
 			break;
 		case "SUPER":
 			this.activeSprite = this.sprite.super;
-			g_FIREBOLTS = 10;
 			break;
 		case "WOW":
 			this.activeSprite = this.sprite.wow;
-			g_FIREBOLTS = 15;
 			break;
 		case "AMAZING":
 			this.activeSprite = this.sprite.amazing;
 			break;
 		case "EXTREME":
 			this.activeSprite = this.sprite.extreme;
-			g_FIREBOLTS = 20;
 			break;
 		case "FANTASTIC":
 			this.activeSprite = this.sprite.fantastic;
 			break;
 		case "SPLENDID":
 			this.activeSprite = this.sprite.splendid;
-			g_FIREBOLTS = 25;
 			break;
 		case "NOWAY":
 			this.activeSprite = this.sprite.noway;
-			g_FIREBOLTS = 50;
 			break;
 	}
 	this.scale = scale;
