df5e61d26326d9cf93491b827ed0cd3885e8e268
Snorri <vaskur_@hotmail.com>
2014-11-19 01:03:27 +0000

    smá breyting á firebolts enn að skoða þetta

--- js/comboLogos.js
+++ js/comboLogos.js
@@ -25,36 +25,42 @@ g_comboLogos.checkCombos = function () {
 	if (plats>5 && plats<=14) {
 		currentNotification.name = "GOOD";
 		currentNotification.scale = 1;
+		g_FIREBOLTS = 5;
 		g_SCORE_MULTIPLIER =1.5;
 		shouldDraw = this.ifDoesntExist(1);
 	}
 	if (plats>14 && plats<=24) {
 		currentNotification.name = "SWEET";
 		currentNotification.scale = 1.3;
+		g_FIREBOLTS = 10;
 		g_SCORE_MULTIPLIER =1.75;
 		shouldDraw = this.ifDoesntExist(2);
 	}
 	if (plats>24 && plats<=34) {
 		currentNotification.name = "GREAT";
 		currentNotification.scale = 1.5;
+		g_FIREBOLTS = 15;
 		g_SCORE_MULTIPLIER =2;
 		shouldDraw = this.ifDoesntExist(3);
 	}
 	if (plats>34 && plats<=50) {
 		currentNotification.name = "SUPER";
 		currentNotification.scale = 1.8;
+		g_FIREBOLTS = 20;
 		g_SCORE_MULTIPLIER =2.5;
 		shouldDraw = this.ifDoesntExist(4);
 	}
 	if (plats>50 && plats<=70) {
 		currentNotification.name = "WOW";
 		currentNotification.scale = 2;
+		g_FIREBOLTS = 25;
 		g_SCORE_MULTIPLIER =3;
 		shouldDraw = this.ifDoesntExist(5);
 	}
 	if (plats>70 && plats<=100) {
 		currentNotification.name = "AMAZING";
 		currentNotification.scale = 2.5;
+		g_FIREBOLTS = 30;
 		g_SCORE_MULTIPLIER =4;
 		shouldDraw = this.ifDoesntExist(6);
 	}
@@ -62,31 +68,38 @@ g_comboLogos.checkCombos = function () {
 		currentNotification.name = "EXTREME";
 		currentNotification.scale = 2.8;
 		g_SCORE_MULTIPLIER =6;
+		g_FIREBOLTS = 35;
 		shouldDraw = this.ifDoesntExist(7);
 	}
 	if (plats>140 && plats<=180) {
 		currentNotification.name = "FANTASTIC";
 		currentNotification.scale = 3;
 		g_SCORE_MULTIPLIER =8;
+		g_FIREBOLTS = 40;
 		shouldDraw = this.ifDoesntExist(8);
 	}
 	if (plats>180 && plats<=250) {
 		currentNotification.name = "SPLENDID";
 		currentNotification.scale = 3.2;
 		g_SCORE_MULTIPLIER =9;
+		g_FIREBOLTS = 45;
 		shouldDraw = this.ifDoesntExist(9);
 	}
 	if (plats>200) {
 		g_SCORE_MULTIPLIER =10;
 		currentNotification.name = "NOWAY";
 		currentNotification.scale = 3.5;
+		g_FIREBOLTS = 50;
+		g_SCORE_MULTIPLIER =9;
 		shouldDraw = this.ifDoesntExist(10);
 	}
 
 	if (!g_COMBO) {
 		this.resetNotification();
+		g_FIREBOLTS = 0;
 		g_SCORE_MULTIPLIER =1;
 	}
+	console.log(g_FIREBOLTS);
 	//return currentNotification;
 	if(shouldDraw) entityManager.generateNotification(currentNotification.name,currentNotification.scale);
 }
